8ee5a9facf276c23d176e773f3ff96e138f25c5d
Jenine Harris <harrisj@wustl.edu>
2022-11-05 16:56:04 -0500

    updates repro course

--- slides/participant-materials/unformatted-r-code.R
+++ slides/participant-materials/unformatted-r-code.R
@@ -5,13 +5,18 @@ library(finalfit)
 library(knitr)
 library(xfun)
 globalData2019<-read_spss("Pew Research Center Global Attitudes Spring 2019 Dataset WEB.sav")
-globalData2019clean<-globalData2019%>%filter(country%in%c(16,21,26,30))%>%select(country,SEX,AGE,BETTER_GENDER,COUNTRY_SATIS,ECON_SIT,LADDER_NOW)%>%zap_labels()%>%
+globalData2019samp <- sample_n(globalData2019,200)
+globalData2019clean<-globalData2019samp%>%select(country,SEX,AGE,BETTER_GENDER,COUNTRY_SATIS,ECON_SIT,LADDER_NOW,POLICY_CLIMATE,POLICY_THEWALL,POLICY_NUCLEAR,POLICY_IMMIG,POLICY_TARRIF,POLICY_NORTHKOREA,POLICY_EMBASSY)%>%zap_labels()%>%
 mutate(AGE=na_if(AGE,98))%>%mutate(AGE=na_if(AGE,99))%>%mutate(LADDER_NOW=na_if(LADDER_NOW,98))%>%
 mutate(LADDER_NOW=na_if(LADDER_NOW,99))%>%mutate(country=recode_factor(country,'16'='Kenya','21'='Nigeria','26'='South Africa','30'='Tunisia'))%>%
 mutate(SEX=recode_factor(SEX,'1'='Male','2'='Female'))%>%mutate(BETTER_GENDER=recode_factor(BETTER_GENDER,'1'='Men','2'='Women','3'='Same/both equally','8'=NA_character_,'9'=NA_character_))%>%
-mutate(COUNTRY_SATIS=recode_factor(COUNTRY_SATIS,'1'='Satisfied','2'='Dissatisfied','8'=NA_character_,'9'=NA_character_))%>%
-mutate(ECON_SIT=recode_factor(ECON_SIT,'1'='Very good','2'='Somewhat good','3'='Somewhat bad','4'='Very bad','8'=NA_character_,'9'=NA_character_))
-summary(globalData2019clean)
+mutate(POLICY_CLIMATE=recode_factor(POLICY_CLIMATE,'1'='Approve','2'='Disapprove','8'=NA_character_,'9'=NA_character_))%>%
+mutate(POLICY_THEWALL=recode_factor(POLICY_THEWALL,'1'='Approve','2'='Disapprove','8'=NA_character_,'9'=NA_character_))%>%
+mutate(POLICY_NUCLEAR=recode_factor(POLICY_NUCLEAR,'1'='Approve','2'='Disapprove','8'=NA_character_,'9'=NA_character_))%>%
+mutate(POLICY_IMMIG=recode_factor(POLICY_IMMIG,'1'='Approve','2'='Disapprove','8'=NA_character_,'9'=NA_character_))%>%
+mutate(POLICY_TARIFF=recode_factor(POLICY_TARRIF,'1'='Approve','2'='Disapprove','8'=NA_character_,'9'=NA_character_))%>%
+mutate(POLICY_NORTHKOREA=recode_factor(POLICY_NORTHKOREA,'1'='Approve','2'='Disapprove','8'=NA_character_,'9'=NA_character_))%>%
+mutate(POLICY_EMBASSY=recode_factor(POLICY_EMBASSY,'1'='Approve','2'='Disapprove','8'=NA_character_,'9'=NA_character_))%>%summary(globalData2019clean)
 label(globalData2019clean$country)<-"Country"
 label(globalData2019clean$SEX)<-"Sex"
 label(globalData2019clean$AGE)<-"Age" 
